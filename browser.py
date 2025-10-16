c5760f9f225743fe896747fb3a8d5b57da8d60d4
Williamnumismatics <williamnumismatics@gmail.com>
2025-10-17 08:28:10 +1100

    Add QToolBar and QAction imports to browser.py
    Imported QToolBar and QAction from PyQt5.QtWidgets in preparation for adding toolbar functionality to the browser application.

--- browser.py
+++ browser.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
 import sys
